b974959c69f9314e4b9748af11fcbf1344b35219
caplewie <149508493+caplewie@users.noreply.github.com>
2023-11-16 12:25:08 -0700

    Update lambda_function.py

--- src/LambdaExample1/lambda_function.py
+++ src/LambdaExample1/lambda_function.py
@@ -1,14 +1,17 @@
 import json
 import hashlib
 import boto3
-from oscar import Oscar
-from oscar import LambdaTimeoutApproaching
+from hasher import Oscar
+from hasher import LambdaTimeoutApproaching
 import ssl
 import random
-recallQueuesMaster=[]
+import time
 maxTTL=4
+##Add 2 queues to write to. Using one queue will be blocked by AWS
+recallQueuesMaster=[]
 
 def lambda_handler(event, context):
+    recallQueues=recallQueuesMaster.copy()
     if len(event['Records']) > 1:
       return {'statusCode':404, 'body':json.dumps('too many events')}
     if event['Records'][0]["eventSource"] == "aws:s3":
@@ -22,6 +25,7 @@ def lambda_handler(event, context):
       restoreData=json.loads(event['Records'][0]['body'])
       bucket=restoreData['bucket']
 #      recallQueues.remove(inQueue.split(':')[-1])
+      recallQueues.remove(inQueue.split(':')[-1])
       ttl=int(restoreData['TTL'])
       key=restoreData['key']
       restoreState=restoreData['state']
@@ -36,7 +40,8 @@ def lambda_handler(event, context):
       if ttl > 0:
         toExport=json.loads(t.export)
         toExport['TTL'] = ttl-1
-        sendSQS(json.dumps(toExport), inQueue)
+
+        sendSQS(json.dumps(toExport), random.choice(recallQueues))
         return {
           'statusCode': 200,
           'body': json.dumps(t.export)
@@ -46,16 +51,22 @@ def lambda_handler(event, context):
           'statusCode': 200,
           'body': json.dumps("TTL EXCEEDED")
         }
-    
+    writeHashFile(key, t.getDigests())
     return {
         'statusCode': 200,
         'body': json.dumps(t.getDigests())
     }
 
-def sendSQS(export, qIn):
-  recallQueues=recallQueuesMaster.copy().remove(qIn.split(':')[-1])
+def sendSQS(export, q):
   sqs = boto3.resource('sqs')
-  queue = sqs.get_queue_by_name(random.choice(recallQueues))
-  print(queue)
+  queue = sqs.get_queue_by_name(QueueName=q)
+#  print(queue)
   response = queue.send_message(MessageBody=export)
   
+def writeHashFile(filename, hashes):
+  bucket="laneslambdatestbucket"
+  s3 = boto3.resource('s3')
+  object = s3.Object(bucket, 'hashes/'+filename+"-"+str(int(time.time()))+'.hash')
+#  object.put(Body=json.dumps(hashes).encode('utf-8'), ObjectLockMode='GOVERNANCE', ObjectLockRetainUntilDate=datetime(2023, 12, 9))
+  object.put(Body=json.dumps(hashes).encode('utf-8'))
+#  object.put(Body='aaaaa')
